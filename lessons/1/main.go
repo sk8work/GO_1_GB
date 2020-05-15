8a61e8c82e00e1fef70ff87d41b47e003cba19e1
sk8work <eshikhantsev@gmail.com>
2020-05-15 20:44:44 +0300

    first lesson

---
+++ lessons/1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"GO_1_GB/lessons/1/packet"
+)
+
+func main() {
+	fmt.Println(packet.CONSTANTA)
+}
---
+++ lessons/1/packet/packet.go
@@ -0,0 +1,4 @@
+package packet
+
+// CONSTANTA just an empty string
+const CONSTANTA = ""
